426a68bc2308e91e8606968abd9c0f4cf6a1b5ff
Fiona Zheng <fiona.zh3ng@gmail.com>
2024-08-25 21:48:38 -0400

    update profile pic

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Open and close sidebar
 function openNav() {
-  document.getElementById("mySidebar").style.width = "60%";
+  document.getElementById("mySidebar").style.width = "50%";
   document.getElementById("mySidebar").style.display = "block";
 }
 
